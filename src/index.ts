1ea3e9c814085f8b1ecaf7b8c052abb29d842e28
SawaGawlau <47000341+SawaGawlau@users.noreply.github.com>
2022-03-28 13:19:03 +0200

    set up

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-class GitHubActions {
-    gitHubActions = async () => {
-        const test: string = 'test'
-        console.log(test)
-        return test
-      }
-}
-export default new GitHubActions()
+import express, { Application } from 'express'
+import { Request, Response } from 'express'
+
+const app: Application = express()
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('HELLO WORLD')
+})
+
+app.listen(3000, () => {
+    console.log('Start aplikacji na porcie 3000!')
+  })
