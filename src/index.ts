402b19e3fd2dff26511c94a4f3906bb4c3d11e11
SawaGawlau <47000341+SawaGawlau@users.noreply.github.com>
2022-03-29 19:38:32 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,14 @@ app.get('/', (req: Request, res: Response) => {
     res.send('HELLO WORLD')
 })
 
+/*
+connect to instance
+create key
+chmod 400 keys
+then:
+ssh -i "~/.ssh/saws-key-pair.pem" ec2-user@ec2-18-197-75-173.eu-central-1.compute.amazonaws.com
+*/
+
 app.listen(3000, () => {
     console.log('Start aplikacji na porcie 3000!')
   })
