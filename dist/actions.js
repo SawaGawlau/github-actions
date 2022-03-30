835ce15274af76d04a0a1d27a342115456d6bc13
SawaGawlau <47000341+SawaGawlau@users.noreply.github.com>
2022-03-30 13:19:16 +0200

    update

---
+++ dist/actions.js
@@ -0,0 +1,3 @@
+console.log('hello');
+console.log('testing github actions for push');
+//# sourceMappingURL=actions.js.map
---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.send('HELLO WORLD');
+});
+/*
+connect to instance
+create key
+chmod 400 keys
+then:
+ssh -i "~/.ssh/saws-key-pair.pem" ec2-user@ec2-18-197-75-173.eu-central-1.compute.amazonaws.com
+*/
+app.listen(3000, () => {
+    console.log('Start aplikacji na porcie 3000!');
+});
+//# sourceMappingURL=index.js.map
