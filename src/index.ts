8de159f5e1c9077635c8a6d51437292014c421fe
SawaGawlau <47000341+SawaGawlau@users.noreply.github.com>
2022-03-30 15:12:22 +0200

    check

--- src/index.ts
+++ src/index.ts
@@ -17,4 +17,5 @@ ssh -i "~/.ssh/saws-key-pair.pem" ec2-user@ec2-18-197-75-173.eu-central-1.comput
 
 app.listen(3000, () => {
     console.log('Start aplikacji na porcie 3000!')
+    console.log('dziala czy nie dziala???')
   })
