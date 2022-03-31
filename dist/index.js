1c72f7004b42f5c6ceba606a87e2a8999a8dc40e
SawaGawlau <47000341+SawaGawlau@users.noreply.github.com>
2022-03-31 16:20:30 +0200

    up

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.get('/', (req, res) => {
-    res.send('push and check');
+    res.send('is it working without manual pull?');
 });
 /*
 connect to instance
