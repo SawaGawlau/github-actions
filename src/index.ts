645cf674c762550fa6d5ea01d0b89059e31246cc
SawaGawlau <47000341+SawaGawlau@users.noreply.github.com>
2022-04-14 13:33:44 +0200

    pm2 check

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express'
 const app: Application = express()
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('checking port 3000')
+    res.send('checking if pm2 is working fine')
 })
 
 /*
