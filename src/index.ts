ea3b65833771d12d5361e26bc3ba918f67f24813
SawaGawlau <47000341+SawaGawlau@users.noreply.github.com>
2022-04-14 11:01:46 +0200

    update port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express'
 const app: Application = express()
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('checking CodeDeploy Pipeline after push...')
+    res.send('checking pm2...')
 })
 
 /*
@@ -15,7 +15,7 @@ then:
 ssh -i "~/.ssh/saws-key-pair.pem" ec2-user@ec2-18-197-75-173.eu-central-1.compute.amazonaws.com
 */
 
-app.listen(4000, () => {
-    console.log('Start aplikacji na porcie 4000!')
+app.listen(3000, () => {
+    console.log('Start aplikacji na porcie 3000!')
     console.log('checking codedeploy-pipeline')
   })
