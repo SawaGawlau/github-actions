f02c6dfd2d36dfc5d4b5d477cef0702eaf09ee53
SawaGawlau <47000341+SawaGawlau@users.noreply.github.com>
2022-04-14 10:05:34 +0200

    check

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express'
 const app: Application = express()
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('checking CodeDeploy Pipeline and new instance...')
+    res.send('checking CodeDeploy Pipeline after push...')
 })
 
 /*
