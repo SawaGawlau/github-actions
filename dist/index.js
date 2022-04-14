957f5858938fd90816bc67bd7fa068f83f97dd94
SawaGawlau <47000341+SawaGawlau@users.noreply.github.com>
2022-04-14 13:34:05 +0200

    check pm2

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.get('/', (req, res) => {
-    res.send('checking port 4000');
+    res.send('checking if pm2 is working fine');
 });
 /*
 connect to instance
