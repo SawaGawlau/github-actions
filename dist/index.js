7537dc2f204920118b7f3643180ee4e7fc0ff4d7
SawaGawlau <47000341+SawaGawlau@users.noreply.github.com>
2022-04-15 09:06:59 +0200

    pm2 check

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.get('/', (req, res) => {
-    res.send('checking if pm2 is working fine');
+    res.send('checking if pm2 is working fine with pm2 start npm --name "app name" -- start');
 });
 /*
 connect to instance
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express'
 const app: Application = express()
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('checking if pm2 is working fine')
+    res.send('checking if pm2 is working fine with pm2 start npm --name "app name" -- start')
 })
 
 /*
