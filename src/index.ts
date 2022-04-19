3f8db329dc0ad863d9ae298bb94c3af9aceab1c6
SawaGawlau <47000341+SawaGawlau@users.noreply.github.com>
2022-04-19 13:33:20 +0200

    checking npm run build

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express'
 const app: Application = express()
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('app deployed from github actions')
+    res.send('checkinf if npm build works fine')
 })
 
 /*
@@ -17,5 +17,5 @@ ssh -i "~/.ssh/saws-key-pair.pem" ec2-user@ec2-18-197-75-173.eu-central-1.comput
 
 app.listen(3000, () => {
     console.log('Start aplikacji na porcie 3000!')
-    console.log('checking update of code and deploy from github actions')
+    console.log('checking npm run build working fine on ec2')
   })
