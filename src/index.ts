430c6ef9e5c10159814f13dfb2b112f25184472c
SawaGawlau <47000341+SawaGawlau@users.noreply.github.com>
2022-05-23 22:49:59 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express'
 const app: Application = express()
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('HELLO world')
+    res.send('HELLO world, restart code deploy agent')
 })
 
 /*
